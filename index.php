4febd107c260e726b93aa24abdad4fea040a5913
VorgSpring <drmrSP@yandex.ru>
2017-04-27 00:58:02 +0300

    brought to HTML from array a list of categories and a list of six ad

--- index.php
+++ index.php
@@ -13,6 +13,46 @@ $remaining_time = $tomorrow - $now;
 
 // оставшееся время в формате (ЧЧ:ММ)
 $lot_time_remaining = gmdate('H: i', $remaining_time);
+
+// категории товаров
+$product_category = ['Доски и лыжи', 'Крепления', 'Ботинки', 'Одежда', 'Инструменты', 'Разное'];
+
+// данные для объявления
+$data_ads = [
+    [
+        'name' => '2014 Rossignol District Snowboard',
+        'category' => 'Доски и лыжи', 'price' => 10999,
+        'image_url' => 'img/lot-1.jpg'
+    ],
+    [
+        'name' => 'DC Ply Mens 2016/2017 Snowboard',
+        'category' => 'Доски и лыжи',
+        'price' => 159999,
+        'image_url' => 'img/lot-2.jpg'
+    ],
+    [
+        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'category' => 'Крепления',
+        'price' => 10999,
+        'image_url' => 'img/lot-3.jpg'],
+    [
+        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'category' => 'Ботинки',
+        'price' => 10999,
+        'image_url' => 'img/lot-4.jpg'
+    ],
+    [
+        'name' => 'Куртка для сноуборда DC Mutiny Charocal',
+        'category' => 'Одежда',
+        'price' => 10999,
+        'image_url' => 'img/lot-5.jpg'],
+    [
+        'name' => 'Маска Oakley Canopy',
+        'category' => 'Разное',
+        'price' => 10999,
+        'image_url' => 'img/lot-6.jpg'
+    ]
+]
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -78,27 +118,35 @@ $lot_time_remaining = gmdate('H: i', $remaining_time);
             <h2>Открытые лоты</h2>
             <select class="lots__select">
                 <option>Все категории</option>
+                <?php
+                    foreach ($product_category as $item) {
+                        echo "<option>$item</option>";
+                    }
+                ?>
             </select>
         </div>
         <ul class="lots__list">
-            <li class="lots__item lot">
-                <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
-                </div>
-                <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="">2014 Rossignol District Snowboard</a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
+            <?php foreach ($data_ads as $item) {
+                echo "<li class='lots__item lot'>
+                        <div class='lot__image'>
+                            <img src=$item[image_url] width='350' height='260' alt='фото товара'>
                         </div>
-                        <div class="lot__timer timer">
-                            <?= $lot_time_remaining; ?>
+                        <div class='lot__info'>
+                            <span class='lot__category'>$item[category]</span>
+                            <h3 class='lot__title'><a class='text-link' href=''>$item[name]</a></h3>
+                            <div class='lot__state'>
+                                <div class='lot__rate'>
+                                    <span class='lot__amount'>Стартовая цена</span>
+                                    <span class='lot__cost'>$item[price]<b class='rub'>р</b></span>
+                                </div>
+                                <div class='lot__timer timer'>
+                                    $lot_time_remaining
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </li>
+                    </li>";
+                }
+            ?>
         </ul>
     </section>
 </main>
