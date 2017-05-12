7e6f7675fdfd082fa4a2945596d8debd4b55e4f0
VorgSpring <drmrSP@yandex.ru>
2017-05-12 22:37:43 +0300

    fixed syntax in queries

--- queries.sql
+++ queries.sql
@@ -12,28 +12,28 @@ SELECT * FROM lots WHERE name LIKE '%name%' OR description LIKE '%описани
 
 // добавить новый лот
 INSERT INTO lots SET
-  date_created = now(),
+  date_created = NOW(),
   name = 'название',
   description = 'описание',
   image_url = 'img/lot.jpg',
-  start_price = '10000',
-  completion_date = date_add(now(), INTERVAL 1 MONTH),
-  step_rate = '100',
-  author_id = '1',
-  category_id = '1';
+  start_price = 10000,
+  completion_date = DATA_ADD(now(), INTERVAL 1 MONTH),
+  step_rate = 100,
+  author_id = 1,
+  category_id = 1;
 
 // обновить название лота по его идентификатору
-UPDATE lots SET name = 'новое название' WHERE id = '1';
+UPDATE lots SET name = 'новое название' WHERE id = 1;
 
 // добавить новую ставку для лота
 INSERT INTO rates SET
-  date = now(),
-  price = '4000',
-  user_id = '1',
-  lot_id = '1'
+  date = NOW(),
+  price = 4000,
+  user_id = 1,
+  lot_id = 1
 
 // получить список ставок для лота по его идентификатору
 SELECT rates.date, rates.price, users.name
 FROM rates
 JOIN users ON rates.user_id = users.id
-WHERE rates.lot_id = '1';
+WHERE rates.lot_id = 1;
